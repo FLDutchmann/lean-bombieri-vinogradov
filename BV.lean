76efeef5db666c57d32e9a8d9e9db6dcf9c7b055
Arend Mellendijk <arend.mellendijk@gmail.com>
2025-11-08 20:55:09 +0100

    init

---
+++ BV.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `BV` library.
+-- Import modules here that should be built as part of the library.
+import BV.Basic
---
+++ Main.lean
@@ -0,0 +1,4 @@
+import BV
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
