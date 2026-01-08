8d9cd00530696f61d8376e885300cb730681c586
Arend Mellendijk <arend.mellendijk@gmail.com>
2026-01-08 14:40:43 +0100

    just do mathlib

--- BV.lean
+++ BV.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `BV` library.
 -- Import modules here that should be built as part of the library.
-import BV.MainResults
+-- import BV.MainResults
+import Mathlib
